6e189567d2c4ea62ff6f8073bc72ba95144625f0
Mukesh <mukesh@EMPID24089.local>
2025-02-06 23:11:00 +0530

    update

--- env/dev/us-west-2/eks/variables.tf
+++ env/dev/us-west-2/eks/variables.tf
@@ -2,22 +2,6 @@ variable "region" {
   type = string
 }
 
-variable "vpc_cidr" {
-  type = string
-}
-
-variable "vpc_name" {
-  type = string
-}
-
-variable "public_subnets" {
-  type = list(string)
-}
-
-variable "private_subnets" {
-  type = list(string)
-}
-
-variable "azs" {
-  type = list(string)
+variable "cluster_name" {
+  type        = string
 }
--- env/dev/us-west-2/vpc/variables.tf
+++ env/dev/us-west-2/vpc/variables.tf
@@ -2,6 +2,22 @@ variable "region" {
   type = string
 }
 
-variable "cluster_name" {
-  type        = string
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
 }
