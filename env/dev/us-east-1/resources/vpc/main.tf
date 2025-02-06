e63fba6103c7b71ead040dbd939f25619c0c24ca
Mukesh <mukesh@EMPID24089.local>
2025-02-06 09:30:05 +0530

    update

--- env/dev/us-east-1/resources/vpc/main.tf
+++ env/dev/us-east-1/resources/vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source          = "https://github.com/devops-contribution/shared-modules.git//modules/vpc?ref=main"
+  source          = "https://github.com/devops-contribution/shared-modules.git//modules/vpc"
   region          = var.region
   vpc_cidr        = var.vpc_cidr
   vpc_name        = var.vpc_name
