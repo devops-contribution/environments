62ab80a672c8fb4a6d87684023a1aba358c00478
Mukesh <mukesh@EMPID24089.local>
2025-02-06 16:12:45 +0530

    update

--- env/dev/us-east-1/resources/vpc/main.tf
+++ env/dev/us-east-1/resources/vpc/main.tf
@@ -12,17 +12,6 @@ provider "aws" {
   }
 }
 
-
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "env/dev/us-east-1/resources/vpc/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock-table"
-  }
-}
-
 module "vpc" {
   source          = "git::https://github.com/devops-contribution/shared-modules.git//modules/vpc?ref=main"
   region          = var.region
