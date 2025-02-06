63fcebd35cc3b1260bb42d61d19ad440dd0d6389
Mukesh <mukesh@EMPID24089.local>
2025-02-06 12:21:05 +0530

    update

--- env/dev/us-east-1/resources/vpc/main.tf
+++ env/dev/us-east-1/resources/vpc/main.tf
@@ -12,12 +12,12 @@ provider "aws" {
   }
 }
 
-#module "vpc" {
-#  source          = "git::https://github.com/devops-contribution/shared-modules.git//modules/vpc?ref=main"
-#  region          = var.region
-#  vpc_cidr        = var.vpc_cidr
-#  vpc_name        = var.vpc_name
-#  public_subnets  = var.public_subnets
-#  private_subnets = var.private_subnets
-#  azs             = var.azs
-#}
+module "vpc" {
+  source          = "git::https://github.com/devops-contribution/shared-modules.git//modules/vpc?ref=main"
+  region          = var.region
+  vpc_cidr        = var.vpc_cidr
+  vpc_name        = var.vpc_name
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  azs             = var.azs
+}
