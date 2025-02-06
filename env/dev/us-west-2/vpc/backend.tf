e7fab8fb4dd28296df9eed102b9f70fbce21fa27
Mukesh <mukesh@EMPID24089.local>
2025-02-06 22:58:06 +0530

    update

--- env/dev/us-west-2/vpc/backend.tf
+++ env/dev/us-west-2/vpc/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket"
+    bucket         = "terraform-remote-be-bucket"
     key            = "vpc/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
