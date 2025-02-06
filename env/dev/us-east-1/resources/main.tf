2a07efe038231d7ce378e0d4b2ea8aa4b177ea38
Mukesh <mukesh@EMPID24089.local>
2025-02-06 18:47:49 +0530

    update

--- env/dev/us-east-1/resources/main.tf
+++ env/dev/us-east-1/resources/main.tf
@@ -26,5 +26,6 @@ module "eks" {
   source           = "git::https://github.com/devops-contribution/shared-modules.git//modules/eks?ref=main"
   region           = var.region
   cluster_name     = var.cluster_name
-  subnet_ids       = module.vpc.public_subnets
+  #subnet_ids       = module.vpc.public_subnets
+  subnet_ids        = ["10.0.1.0/24", "10.0.2.0/24"]
 }
